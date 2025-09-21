a87a82f03be04098d96e1ae6b6c86b4f397f3880
j-sicard <srdjulien@gmail.com>
2025-09-21 21:14:13 +0200

    test for createdhouse

--- src/test/java/com/HomeBooking/HomeBooking/controller/HouseControllerTests.java
+++ src/test/java/com/HomeBooking/HomeBooking/controller/HouseControllerTests.java
@@ -1,5 +1,6 @@
 package com.HomeBooking.HomeBooking.controller;
 
+import com.HomeBooking.HomeBooking.DTO.HouseDTO;
 import com.HomeBooking.HomeBooking.FO.HouseFO;
 import com.HomeBooking.HomeBooking.model.HouseMO;
 import com.HomeBooking.HomeBooking.repository.HouseMongoRepository;
@@ -9,13 +10,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Profile("test")
@@ -80,6 +83,42 @@ public class HouseControllerTests {
                 .containsExactly(
                         org.assertj.core.api.Assertions.tuple("HouseTest", "address Test", 800.0)
                 );
+    }
+
+    @Test
+    void createHouse_shouldReturnNotNullResponse() {
+        ResponseEntity<?> response = houseController.createHouse(houseFoForTest());
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void createHouse_shouldReturnCreatedStatus() {
+        ResponseEntity<?> response = houseController.createHouse(houseFoForTest());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    void createHouse_shouldReturnHouseDto() {
+        ResponseEntity<?> response = houseController.createHouse(houseFoForTest());
+
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody() instanceof HouseDTO);
+    }
+
+
+    @Test
+    void createHouse_shouldReturnHouseDtoWithCorrectValues() {
+        ResponseEntity<?> response = houseController.createHouse(houseFoForTest());
+
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody() instanceof HouseDTO);
+
+        HouseDTO houseDto = (HouseDTO) response.getBody();
+        assertEquals("HouseTest", houseDto.getTitle());
+        assertEquals("address Test", houseDto.getAddress());
+        assertEquals(800.0, houseDto.getPrice());
     }
 
 
